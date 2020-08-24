9bae8010b44329aa6f45961c937df6ef2bb60dd8
wanghaichao <hcwang1024@163.com>
2020-08-24 20:39:07 +0800

    完善测试代码

--- app/src/test/java/com/hc/kotlinstudyexample/todo/tasks/TasksViewModelTest.kt
+++ app/src/test/java/com/hc/kotlinstudyexample/todo/tasks/TasksViewModelTest.kt
@@ -5,6 +5,12 @@ import org.junit.Assert.*
 import org.junit.Test
 import org.junit.runner.RunWith
 import androidx.test.runner.AndroidJUnit4
+import org.junit.Before
+import org.junit.Rule
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.hc.kotlinstudyexample.todo.getOrAwaitValue
+import org.hamcrest.CoreMatchers.*
+
 /**
  * Created by hcw  on 2020/8/23
  * 类描述：
@@ -14,19 +20,39 @@ import androidx.test.runner.AndroidJUnit4
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
 
+    // Subject under test
+    private lateinit var tasksViewModel: TasksViewModel
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Before
+    fun setupViewModel() {
+        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+    }
+
+
     @Test
     fun addNewTask_setsNewTaskEvent() {
-
-        // Given a fresh ViewModel
-        val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
-
         // When adding a new task
         tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
-        // TODO test LiveData
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
 
 
+    }
+
+    @Test
+    fun setFilterAllTasks_tasksAddViewVisible() {
+        // When the filter type is ALL_TASKS
+        tasksViewModel.setFiltering(TasksFilterType.ALL_TASKS)
+
+        // Then the "Add task" action is visible
+        assertThat(tasksViewModel.tasksAddViewVisible.getOrAwaitValue(), `is`(true))
     }
 
 }
